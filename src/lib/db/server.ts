ab6d0b81cc144fe91986f54bea777bc934b51c1e
daniel-clubb <danclubb004@gmail.com>
2025-10-05 21:42:54 +0100

    trying to debug db connection error

--- src/lib/db/server.ts
+++ src/lib/db/server.ts
@@ -1,10 +1,16 @@
 import { Pool } from "pg";
 
+console.log(
+    "Supabase connection string:",
+    process.env.SUPABASE_CONNECTION_STRING
+);
+
 const pool = new Pool({
     connectionString: process.env.SUPABASE_CONNECTION_STRING!,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: true }
+            : { rejectUnauthorized: false },
 });
 
 export function getDbClient() {
